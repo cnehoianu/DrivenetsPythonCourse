6cfd7961433bff7534b5e8b18ab17a6bb16b2103
cnehoianu <cnehoianu@drivenets.com>
2023-02-06 12:01:58 +0200

    first commit message

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+# this is my first code
+print("Hello World!", "I want this to be on the same line", sep="!!!!!")  # added a sep parameter
+print("Hello World2!")
+print("Hello World!", "I want this to be on the same line", sep="anything")  # copy paste 1
